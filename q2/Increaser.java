20921b8c1b5cf2f3f8549b9a3952b16f87162d79
Chris Kimberley <ckimbe02@mail.bbk.ac.uk>
2016-02-21 21:00:51 +0000

    code tidied up and completed

--- q2/Increaser.java
+++ q2/Increaser.java
@@ -1,11 +1,11 @@
 public class Increaser implements Runnable {
-	private SynchronizedCounter c;
+	private Counter c;
 	
-	public Increaser(SynchronizedCounter counter) {
+	public Increaser(Counter counter) {
 		this.c = counter;
 	}
 	public static void main(String args[]) {
-		SynchronizedCounter counter = new SynchronizedCounter();
+		Counter counter = new Counter();
 		for (int i = 0; i < 100; i++) {
 			Increaser increaserTask = new Increaser(counter);
 			Thread t = new Thread(increaserTask);
@@ -16,16 +16,6 @@ public class Increaser implements Runnable {
 	public void run() {
 		System.out.println("Starting at " + c.getCount());
 		for (int i = 0; i < 1000; i++) {
-			try {
-				Thread.sleep(3);
-			}
-			catch(InterruptedException e) {
-				// did nothing !
-				
-			}
-				
-			
-			
 			c.increase();
 		}
 		System.out.println("Stopping at " + c.getCount());
